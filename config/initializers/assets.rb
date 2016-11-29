5fc62c24a1f497a300bafd8f8a03abfda62c363a
Nancy Li <nli@berkeley.edu>
2016-11-29 01:16:50 +0000

    css updates for admins

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( submitpoemform.css )
-Rails.application.config.assets.precompile += %w( adminindex.css )
+Rails.application.config.assets.precompile += %w( admindash.css )
 Rails.application.config.assets.precompile += %w( adminlogin.css )
+Rails.application.config.assets.precompile += %w( adminwelcome.css )
