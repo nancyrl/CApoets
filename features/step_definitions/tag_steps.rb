a8875f8938f7dda2b67664a7af022004eed71028
xurichard <richardxu@berkeley.edu>
2017-07-13 15:49:48 -0700

    fixed failing tests

--- features/step_definitions/tag_steps.rb
+++ features/step_definitions/tag_steps.rb
@@ -1,4 +1,4 @@
-Then /I should see a drop down menu with options: "(.*)"/ do |labels|
+Then /I should see a drop down menu with options : "(.*)"/ do |labels|
     pending
 end
 
@@ -6,10 +6,10 @@ And /I can select "(.*)"/ do |label|
     pending
 end
 
-Given /exists a poem with tags: "(.*)"/ do |labels|
+Given /exists a poem with tags : "(.*)"/ do |labels|
     pending
 end
 
-Then /I should see a poem with tags: "(.*)"/ do |labels|
+Then /I should see a poem with tags : "(.*)"/ do |labels|
     pending
 end
