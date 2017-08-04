36a079798738311cf84b996d66d2815129fb2acf
Winn Cui <winn.yc@berkeley.edu>
2017-08-04 08:59:31 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,15 @@ Rails.application.routes.draw do
   end
 
   root to: 'poems#home'
+  
+  
+  
+  
+  
+  
+  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
