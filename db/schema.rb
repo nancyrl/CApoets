0ae04b7f8cac0fd9879e202a80b335d639cb6cca
Peitian Xiong <peitian.xiong@berkeley.edu>
2017-08-05 16:34:15 -0700

    add preset tags dropdown menu

--- db/schema.rb
+++ db/schema.rb
@@ -34,17 +34,7 @@ ActiveRecord::Schema.define(version: 20170731045219) do
     t.string   "status",               default: "Pending"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "tags"
     t.string   "list_of_tags"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string   "category"
-    t.string   "status"
-    t.integer  "number_of_hits"
-    t.string   "description"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
   end
 
   create_table "tags", force: :cascade do |t|
